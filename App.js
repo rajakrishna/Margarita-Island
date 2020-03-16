315ac900c09af25e21e99efbf7567beb4be9e84f
Raja Krishna <rajakrishna214@gmail.com>
2020-03-16 07:38:49 -0500

    update

--- App.js
+++ App.js
@@ -4,14 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import HomeScreen from './components/HomeScreen'
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import { Fontisto } from 'react-native-vector-icons';
 
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
+
 
 function SettingsScreen() {
   return (
@@ -23,31 +18,42 @@ function SettingsScreen() {
 
 const Tab = createMaterialBottomTabNavigator();
 
-export default function App() {
+const App = () =>{
   return (
     <NavigationContainer>
       <Tab.Navigator 
-        initialRouteName = "Home"
+        initialRouteName = "Settings"
         activeColor = "#e2f3f5"
         inactiveColor = "#3e3636"
         barStyle= {{ backgroundColor: '#3d5af1' }}
         shifting = {true}
         >
 
-
-
         <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({ color }) => (          
             <MaterialCommunityIcons name="home" color={color} size={26} />
           ),
         }}
       />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color }) => (
+            // <Fontisto name="island" color='{color}' size={26} />  
+            <MaterialCommunityIcons name="settings" color={color} size={26} />
+          ),
+        }}
+      />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
