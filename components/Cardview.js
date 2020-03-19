c15b966d52851860e285f8d4c219a4ce34814134
Raja Krishna <rajakrishna214@gmail.com>
2020-03-18 19:27:27 -0500

    Update Cardview.js

--- components/Cardview.js
+++ components/Cardview.js
@@ -4,11 +4,11 @@ import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CardView = () => (
-  <View style={{justifyContent:'center',alignContent:'center',flex:1,paddingLeft:'30%'}} >
+  <View style={{justifyContent:'center',alignContent:'center',paddingLeft: '30%'}} >
     <TouchableOpacity activeOpacity={0.7} >
       <Card  onPress = {() =>{console.log('Hello')}} style={{justifyContent:'center',alignContent:'center', width: 150}}>      
         <Card.Cover source={{ uri: 'https://picsum.photos/700' }} style={{width: 150 , height: 150, borderRadius: 12}}/>
-        <Text style={{justifyContent:'center',alignItems:'center',flex:1}}>hekllo</Text>
+        <Text style={{alignItems:'center',paddingLeft: '38%'}}>hekllo</Text>
       </Card> 
     </TouchableOpacity>
   </View>
