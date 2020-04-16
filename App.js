4048a4f8f858c3c5999a7f837c1b255af88e8b9a
Raja Krishna <rajakrishna214@gmail.com>
2020-04-16 17:10:16 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import BottomNavigation from "./components/BottomNavigation";
 import Firebase from "./components/Firebase";
-import Form from "./components/Form";
 
 const App = () => {
 	return <BottomNavigation />;
