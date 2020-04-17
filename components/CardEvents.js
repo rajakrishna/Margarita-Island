4372abbe03c10f556ac0f6c4e2d035033c6e0684
Raja Krishna <rajakrishna214@gmail.com>
2020-04-16 20:49:28 -0500

    update

--- components/CardEvents.js
+++ components/CardEvents.js
@@ -1,22 +1,21 @@
 import * as React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+// import { white } from "react-native-paper/lib/typescript/src/styles/colors";
 
 const CardEvents = (props) => {
 	return (
-		<Card
-			onPress={props.comk}
-			style={{
-				// borderRadius: 20,
-				width: "97%",
-			}}
-		>
+		<Card onPress={props.comk} style={styles.card}>
 			<Card.Cover source={{ uri: props.link }} />
-			<Card.Title title="Card Title" subtitle="Card Subtitle" />
+			<Card.Title title="Name" subtitle="Location" />
 		</Card>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	card: {
+		marginTop: 10,
+	},
+});
 
 export default CardEvents;
--- components/EventScreen.js
+++ components/EventScreen.js
@@ -1,15 +1,26 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ScrollView, StyleSheet } from "react-native";
 import CardEvents from "./CardEvents";
 
 const EventScreen = () => {
 	return (
-		<View style={{ paddingTop: 100, alignItems: "center" }}>
-			<CardEvents link="https://picsum.photos/700" name="Pho" />
-			<CardEvents link="https://picsum.photos/700" name="Pho" />
-			<CardEvents link="https://picsum.photos/700" name="Pho" />
+		<View style={styles.container}>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<CardEvents link="https://picsum.photos/700" name="Pho" />
+				<CardEvents link="https://picsum.photos/700" name="Pho" />
+				<CardEvents link="https://picsum.photos/700" name="Pho" />
+			</ScrollView>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// backgroundColor: "red",
+		paddingTop: 25,
+		// alignItems: "center",
+	},
+});
 
 export default EventScreen;
