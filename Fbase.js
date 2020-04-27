e040166cb8326930fd35770faf3b96934a9f5c22
Raja Krishna <rajakrishna214@gmail.com>
2020-04-26 22:56:38 -0500

    added-json-file

--- Fbase.js
+++ Fbase.js
@@ -1,60 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ItemComponent from "./Yoyo.js";
 import VerticalListHotel from "./components/VerticalListHotel";
 
 import { db } from "./Config";
 
-let itemsRef = db.ref("/items");
+let itemsRef = db.ref("/items/Hotels");
 
-export default class List extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			// data: [],
-			todos: [],
-		};
-		// console.log(this.state.todos);
-	}
+// export default class List extends Component
+const Fbase = () => {
+	let [todos, setTodos] = useState([]);
 
-	componentDidMount() {
+	useEffect(() => {
 		itemsRef.on("value", (snapshot) => {
 			let data = snapshot.val();
 			let items = Object.values(data);
-			// let ipack = Object.values(items);
-			// let i1 = ipack[0];
-			// let i2 = Object.values(i1);
-			this.setState({ todos: items });
+			setTodos((todos = items));
 		});
-	}
+	});
 
-	render() {
-		const { todos } = this.state;
-		console.log(todos);
-		const renderdata = () => {
-			return todos.map((items, index) => {
-				const { name, location, image } = items;
-				return (
-					<VerticalListHotel
-						key={index}
-						link={image}
-						name={name}
-						location={location}
-					/>
-				);
-			});
-		};
+	// const { todos } = this.state;
+	console.log(todos);
+	const renderdata = () => {
+		return todos.map((items, index) => {
+			const { name, location, image } = items;
+			return (
+				<VerticalListHotel
+					key={index}
+					link={image}
+					name={name}
+					location={location}
+				/>
+			);
+		});
+	};
 
-		// let todokeys = Object.keys(this.state.todos);
-
-		return (
-			<View style={styles.container}>
-				{renderdata()}
-				<Text>Hi</Text>
-			</View>
-		);
-	}
-}
+	return (
+		<View style={styles.container}>
+			{renderdata()}
+			<Text>Hi</Text>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
@@ -63,3 +50,5 @@ const styles = StyleSheet.create({
 		backgroundColor: "#ebebeb",
 	},
 });
+
+export default Fbase;
